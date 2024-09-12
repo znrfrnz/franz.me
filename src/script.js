cc084a5c952cc9a5f531b9d864b2da7e738765b7
znrfrnz <franzbaes69@gmail.com>
2024-09-13 01:15:49 +0800

    added bsod

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.header');
 const drag = document.getElementById('drag');
 const minButton = document.querySelector('.minButton');
 const content = document.querySelector('.content');
-
+// intro draggable and hide //
     header.addEventListener('mousedown', function(e) {
       e.preventDefault();
       
@@ -32,3 +32,53 @@ const content = document.querySelector('.content');
          content.style.display = 'none';
       }
    });
+
+//dangerBTN drag//
+const dangerBTN = document.getElementById('danger');
+const bsod = document.getElementById('bsod');
+
+   if (localStorage.getItem('bsodShown') === 'false') {
+      bsod.style.display = 'flex';
+      enterFullscreen(bsod);
+   }
+
+   dangerBTN.addEventListener('mousedown', function(e) {
+      e.preventDefault();
+         
+      const offsetX = e.clientX - dangerBTN.getBoundingClientRect().left;
+      const offsetY = e.clientY - dangerBTN.getBoundingClientRect().top;
+
+      function onMouseMove(e) {
+        dangerBTN.style.left = `${e.clientX - offsetX}px`;
+        dangerBTN.style.top = `${e.clientY - offsetY}px`;
+      }
+
+      function onMouseUp() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+   });
+      
+   dangerBTN.addEventListener('click', function() {
+         bsod.style.display = 'flex';
+         localStorage.setItem('bsodShown', 'true');
+         enterFullscreen(bsod);
+   });
+
+   // bsod.addEventListener('click', function() {
+   // });
+
+   function enterFullscreen(element) {
+      if (element.requestFullscreen) {
+         element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+         element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) { 
+         element.webkitRequestFullscreen();
+       } else if (element.msRequestFullscreen) {
+         element.msRequestFullscreen();
+       }
+     }
